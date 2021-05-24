74ba62752475aaa19007d737567e667f3747e174
hallux <halluxdev@gmail.com>
2021-05-24 19:11:09 -0400

    Update FinalView.swift

--- FinalView.swift
+++ FinalView.swift
@@ -1,1 +1,16 @@
+// This will house the final output of calories and the sliders. Once Calculate is pressed on SecondView, it'll dump over to this view with that data.
 
+
+import SwiftUI
+
+struct FinalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FinalView_Previews: PreviewProvider {
+    static var previews: some View {
+        FinalView()
+    }
+}
